81b320d6f97fb68b681bc315223f05e852e26f67
philo <phil@stormid.com>
2016-03-02 14:26:11 +0000

    Adds NamespaceDoc class

---
+++ src/Cake.Gulp/Properties/Namespaces.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+// ReSharper disable once CheckNamespace
+namespace Cake.Gulp
+{
+    /// <summary>
+    /// This namespace contain types used for operations using the Npm tool wrapper
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+    }
+}
