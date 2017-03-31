19176700d218162af6df3ff42932f2c79abbd77e
Gary Ewan Park <gep13@gep13.co.uk>
2017-03-31 19:53:16 +0000

    AppVeyor Publish: 72802e045fcfa7ee100f92f599ed77473c4e7036 Merge branch 'release/0.7.0'
    * release/0.7.0:
      (GH-11) Finished blog post
      (GH-11) Added Wyam Documentation
      (GH-8) Corrected cache invalidation
      (GH-8) Updated AppVeyor config file
      (GH-8) Refactoring to get basic build to work
      (GH-8) Updated gitignore file
      (GH-8) Added main Cake.Recipe files
      (GH-8) Removed GitVersion Config file
      (GH-8) Added GRM Config file
      (GH-8) Renamed AppVeyor file

---
+++ assets/js/searchIndex.js
@@ -0,0 +1,140 @@
+
+var camelCaseTokenizer = function (obj) {
+    var previous = '';
+    return obj.toString().trim().split(/[\s\-]+|(?=[A-Z])/).reduce(function(acc, cur) {
+        var current = cur.toLowerCase();
+        if(acc.length === 0) {
+            previous = current;
+            return acc.concat(current);
+        }
+        previous = previous.concat(current);
+        return acc.concat([current, previous]);
+    }, []);
+}
+lunr.tokenizer.registerFunction(camelCaseTokenizer, 'camelCaseTokenizer')
+var searchModule = function() {
+    var idMap = [];
+    function y(e) { 
+        idMap.push(e); 
+    }
+    var idx = lunr(function() {
+        this.field('title', { boost: 10 });
+        this.field('content');
+        this.field('description', { boost: 5 });
+        this.field('tags', { boost: 50 });
+        this.ref('id');
+        this.tokenizer(camelCaseTokenizer);
+
+        this.pipeline.remove(lunr.stopWordFilter);
+        this.pipeline.remove(lunr.stemmer);
+    });
+    function a(e) { 
+        idx.add(e); 
+    }
+
+    a({
+        id:0,
+        title:"GulpLocalRunner",
+        content:"GulpLocalRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:1,
+        title:"GulpGlobalRunner",
+        content:"GulpGlobalRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:2,
+        title:"GulpLocalRunnerSettings",
+        content:"GulpLocalRunnerSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:3,
+        title:"GulpRunner",
+        content:"GulpRunner",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:4,
+        title:"GulpRunnerFactory",
+        content:"GulpRunnerFactory",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:5,
+        title:"GulpRunnerSettings",
+        content:"GulpRunnerSettings",
+        description:'',
+        tags:''
+    });
+
+    a({
+        id:6,
+        title:"GulpRunnerAliases",
+        content:"GulpRunnerAliases",
+        description:'',
+        tags:''
+    });
+
+    y({
+        url:'/Cake.Gulp/Cake.Gulp/api/Cake.Gulp/GulpLocalRunner',
+        title:"GulpLocalRunner",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Gulp/Cake.Gulp/api/Cake.Gulp/GulpGlobalRunner',
+        title:"GulpGlobalRunner",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Gulp/Cake.Gulp/api/Cake.Gulp/GulpLocalRunnerSettings',
+        title:"GulpLocalRunnerSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Gulp/Cake.Gulp/api/Cake.Gulp/GulpRunner_1',
+        title:"GulpRunner<TSettings>",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Gulp/Cake.Gulp/api/Cake.Gulp/GulpRunnerFactory',
+        title:"GulpRunnerFactory",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Gulp/Cake.Gulp/api/Cake.Gulp/GulpRunnerSettings',
+        title:"GulpRunnerSettings",
+        description:""
+    });
+
+    y({
+        url:'/Cake.Gulp/Cake.Gulp/api/Cake.Gulp/GulpRunnerAliases',
+        title:"GulpRunnerAliases",
+        description:""
+    });
+
+    return {
+        search: function(q) {
+            return idx.search(q).map(function(i) {
+                return idMap[i.ref];
+            });
+        }
+    };
+}();
