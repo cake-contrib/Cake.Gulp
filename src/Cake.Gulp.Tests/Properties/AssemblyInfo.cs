d88b77f50324f389305eda6e6e19a5629861f1ab
Simar <theramis@users.noreply.github.com>
2018-10-03 00:03:47 +1300

    (GH-24) Adds a target to .netstandard 2.0 (#27)
    Target .NET Standard 2.0 updated current support from .net46 to .net461

--- src/Cake.Gulp.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Cake.Gulp.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
